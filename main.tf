be46b7a18b7aecf03df95be976eb52b047d35f99
Mahesh <mrdevops1@gmail.com>
2026-07-19 19:29:24 +0530

    added s3

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket       = "mrdevops12-terraform-state-2026"
+    key          = "ec2/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
